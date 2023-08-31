a50725fb00cf090a91d3a6de3b4ec0d2eec7af4d
eitan Benita <e.benita@lyon.ort.asso.fr>
2023-08-31 13:59:31 +0200

    Interface (sans les cadres) + Progr jusqu'au 3

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [nbSaisi, setNbSaisi] = useState('');
+
+  const max = 11;
+  const random = Math.floor(Math.random()*max);
+  console.log(random);
+  const [nb, setNb] = useState(random);
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  //setCount(0);
+
   return (
     <View style={styles.container}>
-      <Text>Rattrapage</Text>
+      <View>
+        <Text style={styles.titre}>Rattrapage</Text>
+        </View>
+      <View>
+        <Text>Tentatives</Text>
+        <Text 
+        defaultValue={count}/>
+      </View>
+      <View>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Rentrer un nombre"
+        onChangeText={newNbSaisi => setNbSaisi(newNbSaisi)}
+        defaultValue={nbSaisi}
+      />
+      </View>
+      <View>
+      <Button
+        title="Confirmer"
+        onPress={() => setCount(count + 1)}
+      />
+      </View>
+      <View>
+        <Text>C'est plus</Text>
+        <Text>C'est moins</Text>
+      </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +55,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titre: {
+    fontSize: 30,
   },
 });
